98831c87bd07916b7673a6d1b9b2d823a791d874
lls101 <liliangshuo101@outlook.com>
2025-02-13 23:41:10 +0800

    add -mno-relax

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ QEMU=./qemu-system-riscv32
 
 # clang 路径和编译器标志
 CC=clang  # Ubuntu 用户：使用 CC=clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall  -mno-relax  -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # 构建内核
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
